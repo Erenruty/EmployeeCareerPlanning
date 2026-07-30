220663fe159ac55b5f404a795d6e8e18f1205b2d
Eren_ <erengunes77@gmail.com>
2026-07-30 23:47:41 +0300

    Add Entity Classes

--- CareerManagamentSystem/Controllers/HomeController.cs
+++ CareerManagamentSystem/Controllers/HomeController.cs
@@ -3,13 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using CareerManagamentSystem.Models;
 namespace CareerManagamentSystem.Controllers
 {
     public class HomeController : Controller
     {
         public ActionResult Index()
         {
+            CareerSystemEntities1 db = new CareerSystemEntities1();
             return View();
         }
 
